b0d7e433bb6821131bde9e1ef32533d7c19d2256
Erzhan Zhusupov <zzhusupoverzhan@gmail.com>
2025-06-14 11:28:51 -0400

    second

--- pars.py
+++ pars.py
@@ -11,7 +11,7 @@ def scrolldown(driver, times=50):
 
 def create_driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Можно убрать для визуализации
+    chrome_options.add_argument("--headless")  
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     service = Service("/opt/homebrew/bin/chromedriver")
